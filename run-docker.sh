43b894bd23188389494b79b931fc0a98083aa9ca
sepehr <sepehr.ha@gmail.com>
2024-08-09 15:05:04 +0330

    development docker running script updated

--- run-docker.sh
+++ run-docker.sh
@@ -1,1 +1,25 @@
-sudo docker run --rm -it --net host --name mikroman-dev --add-host=host.docker.internal:host-gateway -v /opt/mikrowizard/:/conf/ -v /app/migrations/:/app/migrations/ -v /app/py:/app/py -v /app/firms:/app/firms -v /app/back:/backups/ -v /app/reload:/app/reload mikroman:latest uwsgi --ini /app/conf/uwsgi.ini:uwsgi-docker-dev --touch-reload=/app/reload
+#check if producttion docker exest and stop it
+if [ "$(sudo docker ps -a -q -f name=mikroman)" ]; then
+    if [ "$(sudo docker ps -aq -f status=running -f name=mikroman)" ]; then
+        # cleanup
+        echo "stoping production docker"
+        sudo docker container stop mikroman
+    fi
+    # run your container
+fi
+
+mydir=$(pwd)
+pydir="${mydir}/py"
+dbmigratedir="${mydir}/migrations"
+firmdir="${mydir}/firms"
+backupdir="${mydir}/backups"
+reloaddir="${mydir}/reload"
+echo "Creating backup and firms dir in current dir"
+mkdir -p $firmdir
+mkdir -p $backupdir
+echo $firmdir
+if [ -d /opt/mikrowizard/ ]; then
+  echo "running and creating mikroman dev container."
+  sudo docker run --rm -it --net host --name mikroman-dev --add-host=host.docker.internal:host-gateway -v /opt/mikrowizard/:/conf/ -v $dbmigratedir:/app/migrations/ -v $pydir:/app/py -v $firmdir:/firms -v $backupdir:/backups -v $reloaddir:/app/reload mikrowizard/mikroman:latest uwsgi --ini /app/conf/uwsgi.ini:uwsgi-docker-dev --touch-reload=/app/reload
+fi
+
